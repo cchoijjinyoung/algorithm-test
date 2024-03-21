a6617f09f815c50f9efd1eef611f000acca98008
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-03-21 22:37:33 +0900

    [level 2] Title: 귤 고르기, Time: 37.05 ms, Memory: 93.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for (int i = 0; i < tangerine.length; i++) {
+            hm.put(tangerine[i], hm.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        int[] boxes = new int[hm.size()];
+        int index = 0;
+        
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            boxes[index] = entry.getValue();
+            index++;
+        }
+        Arrays.sort(boxes);
+        
+        int removeBox = 0;
+        int removeCnt = tangerine.length - k;
+        for (int i = 0; i < boxes.length; i++) {
+            removeCnt -= boxes[i];
+            if (removeCnt < 0) {
+                removeCnt = i;
+                break;
+            }
+        }
+        answer = boxes.length - removeCnt;
+        return answer;
+    }
+}
