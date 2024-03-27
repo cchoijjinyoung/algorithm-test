2433e8f11640922b196278e23cbf870b7b5c7d6c
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-03-27 19:39:28 +0900

    [Silver V] Title: 문서 검색, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine());
+        String find = br.readLine();
+
+        while (sb.length() >= find.length()) {
+            int idx = sb.indexOf(find);
+            if (idx != -1) {
+                sb.replace(0, idx + find.length(), "");
+                // System.out.println(sb.toString());
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
