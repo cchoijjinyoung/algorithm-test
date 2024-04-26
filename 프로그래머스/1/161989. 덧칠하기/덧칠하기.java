cccb0dc56ab2cce8260834d135bebf43aeaf6f45
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-04-26 14:31:36 +0900

    [level 1] Title: 덧칠하기, Time: 1.55 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,27 +1,14 @@
-import java.util.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        
-        int[] wall = new int[n + 1];
-        Arrays.fill(wall, 1);
-        
-        for (int i = 0; i < section.length; i++) {
-            wall[section[i]] = 0;
-        }
-        
-        for (int i = 1; i < wall.length; i++) {
-            if (wall[i] == 0) {
-                for (int j = 0; j < m; j++) {
-                    if (i + j > n) {
-                        break;
-                    }
-                    wall[i + j] = 1;    
-                }
-                answer++;
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
             }
         }
-        return answer;
+        return cnt;
     }
 }
