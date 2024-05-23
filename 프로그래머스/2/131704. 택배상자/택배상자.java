572f4a3c7f57105887908204bc2377830cda442c
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-05-23 13:25:28 +0900

    [level 2] Title: 택배상자, Time: 184.50 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,26 +1,51 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        int cnt = 0;
+        // order 배열을 변환시켜줌(43125 -> 34215)
+        int[] temp = new int[order.length];
 
-        Stack<Integer> 보조 = new Stack<>();
-        Queue<Integer> 기존 = new LinkedList<>();
+        for (int i = 0; i < order.length; i++) {
+            int idx = order[i] - 1;
+            temp[idx] = i + 1;
+        }
+        // 컨테이너 벨트
+        Queue<Integer> cb = new LinkedList<>();
+        for (int i : temp) {
+            cb.add(i);
+        }
 
-            for(int i=0; i<order.length; i++){
-                보조.add(i+1);
+        // 보조 컨테이너 벨트
+        Stack<Integer> sb = new Stack<>();
 
-                while(!보조.isEmpty()){
-                    if(보조.peek() == order[cnt]){
-                        기존.offer(보조.pop());
-                        cnt++;
-                    }else break;
+        // 기대하는 다음 택배상자 번호
+        int expectNumber = 1;
 
+        while (cb.size() > 0 || sb.size() > 0) {
+            if (cb.size() == 0 && sb.peek() != expectNumber) {
+                break;
+            }
+            // 보조 컨벨 확인
+            if (sb.size() > 0 && sb.peek() == expectNumber) {
+                sb.pop();
+                answer++;
+                expectNumber++;
+                continue;
+            }
+
+            // 컨벨 확인
+            if (cb.size() > 0) {
+                if (cb.peek() == expectNumber) {
+                    cb.poll();
+                    answer++;
+                    expectNumber++;
+                } else {
+                    sb.push(cb.poll());
                 }
             }
-        // System.out.println("보조 : " + 보조.toString());
-        // System.out.println("기존 : " + 기존.toString());
-        answer = 기존.size();
+        }
+
         return answer;
     }
 }
