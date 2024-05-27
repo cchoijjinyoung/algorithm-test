d2d56f5828353295be80618ce43705b80c7b9abb
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-05-27 23:32:05 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.06 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < k; i++) {
+            sb.append(my_string);
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
