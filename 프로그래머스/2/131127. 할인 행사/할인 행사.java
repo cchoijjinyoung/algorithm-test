1fd5d4bac674b265fe0585e05e4a0bd1d2dcdfa0
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-05-28 15:52:30 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,69 +1,52 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int[] correct = new int[want.length];
-        
-        // 맵에
-        Map<String, Integer> hm = new HashMap<>();
-        
-        // 재료의 인덱스를 담고있어.
-        Map<String, Integer> idx = new HashMap<>();
-        
-        // 재료와 필요한 갯수를 담아
-        for (int i = 0; i < want.length; i++) {
-            hm.put(want[i], number[i]);
+                
+        // 초기화 
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<want.length; i++) {
+                        map.put(want[i], number[i]);
         }
-        
-        // 담아주고,
-        for (int i = 0; i < want.length; i++) {
-            idx.put(want[i], i);
-        }
-        
-        // discount 순회하면서 갯수에서 한개씩 빼, 10개
-        for (int i = 0; i < 10; i++) {
-            hm.put(discount[i], hm.getOrDefault(discount[i], 0) - 1);
-            // want에 없으면 0으로 들어간다.
-            if (idx.containsKey(discount[i])) {
-                if (hm.get(discount[i]) <= 0) {
-                    correct[idx.get(discount[i])] = 1;
-                } else {
-                    correct[idx.get(discount[i])] = 0;
-                }
+                
+        // 10번 계산
+        for(int i=0; i<10; i++) {
+            if(map.containsKey(discount[i])) {  
+                                map.put(discount[i], map.get(discount[i]) -1);              
             }
-            
-            if (Arrays.stream(correct).sum() == want.length) {
+        }
+                
+        if(check(map)) {
+            answer++;
+        }
+              
+        int p1 = 0;
+        int p2 = 10; 
+        while(p2 <= discount.length-1) {
+                        
+            if(map.containsKey(discount[p1])) {
+                                map.put(discount[p1], map.get(discount[p1]) + 1);
+            }
+                        
+            if(map.containsKey(discount[p2])) {
+                                map.put(discount[p2], map.get(discount[p2]) - 1);
+            }
+                        
+            if(check(map)) {
                 answer++;
             }
+            p1++;
+            p2++;
         }
-        
-        // 앞에꺼는 재료 + 1, 뒤에꺼는 재료 - 1
-        for (int i = 10; i < discount.length; i++) {
-            hm.put(discount[i - 10], hm.getOrDefault(discount[i - 10], 0) + 1);
-            
-            if (idx.containsKey(discount[i - 10])) {
-                if (hm.get(discount[i - 10]) <= 0) {
-                    correct[idx.get(discount[i - 10])] = 1;
-                } else {
-                    correct[idx.get(discount[i - 10])] = 0;
-                }
-            }
-            
-            hm.put(discount[i], hm.getOrDefault(discount[i], 0) - 1);
-            if (idx.containsKey(discount[i])) {
-                if (hm.get(discount[i]) <= 0) {
-                    correct[idx.get(discount[i])] = 1;
-                } else {
-                    correct[idx.get(discount[i])] = 0;
-                }
-            }
-            
-            if (Arrays.stream(correct).sum() == want.length) {
-                answer++;
-            }
-        }
-        
         return answer;
     }
+        
+    public boolean check(Map<String, Integer> map) {
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() !=0) {
+                return false;
+            }
+        }
+        return true;
+    }        
 }
