a926d823eaf77f0135dba1d971c0caadb848383d
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-05-28 18:28:51 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 53.56 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -3,33 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
+        long sum = 0;
+        long sum1 = 0;
         
         Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q2 = new LinkedList<>();
         
         for (int i : queue1) {
+            sum += i;
+            sum1 += i;
             q1.add(i);
         }
         
         for (int i : queue2) {
+            sum += i;
             q2.add(i);
         }
         
-        long sum1 = Arrays.stream(queue1).sum();
-        long sum2 = Arrays.stream(queue2).sum();
-        
-        if ((sum1 + sum2) % 2 == 1) {
+        if (sum % 2 == 1) {
             return -1;
         }
         
-        long target = (sum1 + sum2) / 2;
+        long target = sum / 2;
         
-        for (int i = 0; i < 2 * (queue1.length + queue2.length); i++) {
+        for (int i = 0; i < 3 * (queue1.length + queue2.length); i++) {
             if (sum1 < target && !q2.isEmpty()) {
                 int num = q2.poll();
                 q1.add(num);
                 sum1 += num;
-                sum2 -= num;
                 answer++;
                 continue;
             }
@@ -37,7 +38,6 @@ class Solution {
             if (sum1 > target && !q1.isEmpty()) {
                 int num = q1.poll();
                 q2.add(num);
-                sum2 += num;
                 sum1 -= num;
                 answer++;
                 continue;
@@ -48,8 +48,7 @@ class Solution {
             }
         }
         
-        if (sum1 != sum2) answer = -1;
-
+        if (sum1 != target) answer = -1;
         return answer;
     }
 }
