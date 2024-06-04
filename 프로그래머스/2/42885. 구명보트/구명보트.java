3dbb963b80f63d04841f12b6b5d1b7c1e8b639ff
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-06-04 16:04:15 +0900

    [level 2] Title: 구명보트, Time: 9.05 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int weight = 0;
+        int min = 0;
+        for (int i = people.length - 1; i >= min; i--) {
+            if (people[i] + people[min] <= limit) {
+                answer++;
+                min++;
+            } else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
