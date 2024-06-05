4ce3f328f7f8f0777ed8ea0bc796b3b9b28dc909
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-06-05 11:43:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 53.46 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack();
+        
+        for (int num : arr) {
+            if (stack.isEmpty()) {
+                stack.push(num);
+                continue;
+            }
+            
+            if (stack.peek() == num) {
+                continue;
+            } else {
+                stack.push(num);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for (int i = answer.length - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
