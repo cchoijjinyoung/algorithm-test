031c5a23a48fffa02777da64134f869c04fa8b5a
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-07-20 23:01:23 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 24.83 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> waiting = new LinkedList<>();
-        Queue<Truck> bridge = new LinkedList<>();
+        Queue<int[]> bridge = new LinkedList<>();
         
         // 트럭을 줄 세운다.
         for (int truck : truck_weights) {
@@ -19,9 +19,9 @@ class Solution {
             
             // 1초 후 나가는 트럭 poll
             if (!bridge.isEmpty()) {
-                if (bridge.peek().endTime == curTime) {
-                    Truck truck = bridge.poll();
-                    curWeight -= truck.weight;
+                if (bridge.peek()[1] == curTime) {
+                    int[] truck = bridge.poll();
+                    curWeight -= truck[0];
                 }
             }
             
@@ -29,20 +29,11 @@ class Solution {
             int waitTruck = waiting.peek();
             if (curWeight + waitTruck <= weight && bridge.size() < bridge_length) {
                 waiting.poll();
-                bridge.add(new Truck(waitTruck, curTime + bridge_length));
+                bridge.add(new int[]{waitTruck, curTime + bridge_length});
                 curWeight += waitTruck;
             }
         }
         return curTime + bridge_length;
     }
                     
-    class Truck {
-        int weight;
-        int endTime;
-        
-        public Truck(int weight, int endTime) {
-            this.weight = weight;
-            this.endTime = endTime;
-        }
-    }
 }
