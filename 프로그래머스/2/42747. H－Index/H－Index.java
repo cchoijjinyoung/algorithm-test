ecdbee12d49747fe19f1faab14650585f2bd367c
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-07-21 00:23:06 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        
+        int len = citations.length;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= len) {
+                answer = len;
+                break;
+            }
+            len--;
+        }
+        return answer;
+    }
+}
