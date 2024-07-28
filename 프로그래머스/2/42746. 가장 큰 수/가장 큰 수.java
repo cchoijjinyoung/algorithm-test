96c271f84315f99d2696fae5b1aa4b02aec94e2e
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-07-29 00:20:01 +0900

    [level 2] Title: 가장 큰 수, Time: 2757.40 ms, Memory: 406 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -10,14 +10,13 @@ class Solution {
             temp[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(temp, new Comparator<String>() {
-            public int compare(String s1, String s2) {
+        Arrays.sort(temp, (s1, s2) -> {
                 // 3, 30
                 // 330, 303
                 int sum1 = Integer.parseInt(s1 + s2);
                 int sum2 = Integer.parseInt(s2 + s1);
                 return sum2 - sum1;
-            }
+            
         });
         
         for (int i = 0; i < temp.length; i++) {
