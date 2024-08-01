a01bec5c7402e495680541e6a0f695ef119dd7d2
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-01 10:15:28 +0900

    [level 2] Title: 요격 시스템, Time: 508.32 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,16 +1,21 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        int answer = 0;
+        int answer = 1;
         
-        Arrays.sort(targets, (a, b) -> a[1] - b[1]);
+        Arrays.sort(targets, (target1, target2) -> target1[0] == target2[0] ? target1[1] - target2[1] : target1[0] - target2[0]);
         
-        int before = 0;
+        int start = targets[0][0];
+        int end = targets[0][1];
         for (int i = 0; i < targets.length; i++) {
-            if (before <= targets[i][0]) {
-                before = targets[i][1];
+            if (targets[i][0] >= end) {
                 answer++;
+                start = targets[i][0];
+                end = targets[i][1];
+            } else {
+                start = targets[i][0];
+                end = Math.min(end, targets[i][1]);
             }
         }
         return answer;
