f106893f5337f8881b8f2ccae4d6825bc04a2713
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-04 11:10:47 +0900

    [level 2] Title: 광물 캐기, Time: 1.26 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -3,66 +3,81 @@ import java.util.*;
 class Solution {
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        int[][] tired = new int[][]{
-            {1, 1, 1},
-            {5, 1, 1},
-            {25, 5, 1}
-        };
         
-        List<Mineral> list = new ArrayList<>();
-        int size = Arrays.stream(picks).sum();
-        
-        for (int i = 0; i < size; i++) {
-            int diamond = 0;
-            int iron = 0;
-            int stone = 0;
-            
-            for (int j = i * 5; j < i * 5 + 5; j++) {
-                if (j >= minerals.length) break;
-                
-                if (minerals[j].startsWith("d")) {
-                    diamond += tired[0][0];
-                    iron += tired[1][0];
-                    stone += tired[2][0];
-                } else if (minerals[j].startsWith("i")) {
-                    diamond += tired[0][1];
-                    iron += tired[1][1];
-                    stone += tired[2][1];
-                } else if (minerals[j].startsWith("s")) {
-                    diamond += tired[0][2];
-                    iron += tired[1][2];
-                    stone += tired[2][2];
-                }
-            }
-            list.add(new Mineral(diamond, iron, stone));
+        int len = 0;
+        for (int pick : picks) {
+            len += pick;
         }
         
-        list.sort((a, b) -> b.stone - a.stone);
-
-        for (int i = 0; i < list.size(); i++) {
-            if (picks[0] > 0) {
-                answer += list.get(i).diamond;
+        // 5개 단위로 끊어서 돌 곡괭이로 캤을 때, 점수를 센다.
+        // 1~5 -> 20, 6~9, 10 -> 10~14 -> 40
+        // 0
+        // 40, 10
+        // {20, 25, 30};
+        // 내림차순 정렬해서 다이아 곡괭이가 있으면 다이아로, 철 곡괭이가 있으면 철 곡괭이로 캔다. 돌 ~
+        PriorityQueue<Mineral> pq = new PriorityQueue<>((m1, m2) -> m2.sum - m1.sum);
+        
+        int sum = 0;
+        
+        int diaCnt = 0;
+        int ironCnt = 0;
+        int stoneCnt = 0;
+        
+        int min = Math.min(len * 5, minerals.length);
+        int count = 0;
+        for (int i = 0; i < min; i++) {
+            count++;
+            if ("diamond".equals(minerals[i])) {
+                diaCnt++;
+                sum += 25;
+            } else if ("iron".equals(minerals[i])) {
+                ironCnt++;
+                sum += 5;
+            } else {
+                stoneCnt++;
+                sum += 1;
+            }
+            
+            if (count % 5 == 0 || i == min - 1) {
+                pq.offer(new Mineral(diaCnt, ironCnt, stoneCnt, sum));
+                diaCnt = 0; ironCnt = 0; stoneCnt = 0;
+                sum = 0; // 합산 초기화
+            }
+        }
+        
+        while (!pq.isEmpty()) {
+            // pq에서 꺼낸다.
+            Mineral cur = pq.poll();
+            int dia = cur.dia;
+            int iron = cur.iron;
+            int stone = cur.stone;
+            
+            if (picks[0] != 0) {
+                answer += dia + iron + stone;
                 picks[0]--;
-            } else if (picks[1] > 0) {
-                answer += list.get(i).iron;
+            } else if (picks[1] != 0) {
+                answer += dia * 5 + iron + stone;
                 picks[1]--;
-            } else if (picks[2] > 0) {
-                answer += list.get(i).stone;
+            } else {
+                answer += dia * 25 + iron * 5 + stone;
                 picks[2]--;
             }
         }
+        
         return answer;
     }
-        
-    class Mineral {
-        int diamond;
+    
+    public class Mineral {
+        int dia;
         int iron;
         int stone;
+        int sum;
         
-        public Mineral(int diamond, int iron, int stone) {
-            this.diamond = diamond;
+        public Mineral(int dia, int iron, int stone, int sum) {
+            this.dia = dia;
             this.iron = iron;
             this.stone = stone;
+            this.sum = sum;
         }
     }
 }
