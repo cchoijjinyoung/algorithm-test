19e9b8af154f4172a99ad265fd05c4cac8aa9010
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-05 12:06:56 +0900

    [level 2] Title: 미로 탈출, Time: 11.82 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,99 +1,77 @@
 import java.util.*;
 
 class Solution {
-    // 상 하 좌 우
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-    
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
     public int solution(String[] maps) {
         int answer = 0;
-        int[] lever = new int[2];
-        int[] start = new int[2];
-        int[] exit = new int[2];
-        // start -> 레버 최소 수 + 레버 -> 문 최소 수
         
+        int[] S = new int[2];
+        int[] E = new int[2];
+        int[] L = new int[2];
+    
+        // S, E, L 을 찾는다.
         for (int i = 0; i < maps.length; i++) {
-            char[] arr = maps[i].toCharArray();
-            
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == 'L') {
-                    lever[0] = i;
-                    lever[1] = j;
-                } else if (arr[j] == 'S') {
-                    start[0] = i;
-                    start[1] = j;
-                } else if (arr[j] == 'E') {
-                    exit[0] = i;
-                    exit[1] = j;
+            for (int j = 0; j < maps[i].length(); j++) {
+                char c = maps[i].charAt(j);
+                
+                if (c == 'S') {
+                    S[0] = i; S[1] = j;
+                } else if (c == 'E') {
+                    E[0] = i; E[1] = j;
+                } else if (c == 'L') {
+                    L[0] = i; L[1] = j;
                 }
             }
         }
-        int a = find(start, lever, maps);
-        int b = find(lever, exit, maps);
-        if (a == Integer.MAX_VALUE || b == Integer.MAX_VALUE) {
-            answer = -1;
-        } else {
-            answer = a + b;
+        
+        int SL = BFS(maps, S, L);
+        int LE = BFS(maps, L, E);
+        
+        if (SL == -1 || LE == -1) {
+            return -1;
         }
-        return answer;
+        
+        return SL + LE;
     }
     
-    public int find(int[] d1, int[] d2, String[] maps) {
-        Point start = new Point(d1[0], d1[1]);
-        Queue<Point> q = new LinkedList<>();
-        int[][] visited = new int[maps.length][maps[0].length()];
+    public int BFS(String[] maps, int[] start, int[] end) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length()];
+        q.offer(new int[]{start[0], start[1], 0});
         
-        for (int i = 0; i < visited.length; i++) {
-            Arrays.fill(visited[i], Integer.MAX_VALUE);
-        }
-        
-        q.offer(start);
-        visited[start.x][start.y] = 0;
-        
+        // S에서 L로 가는 최소 거리를 구한다.
         while (!q.isEmpty()) {
-            // 4방향으로 이동한다.
-            // 다음 위치가 벽이나 밖이면 continue;
-            Point cur = q.poll();
-            int cnt = visited[cur.x][cur.y];
+            int[] cur = q.poll();
+            
+            if (visited[cur[0]][cur[1]]) {
+                continue;
+            }
+            
+            visited[cur[0]][cur[1]] = true;
+            
+            int dist = cur[2];
             
             for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
                 
-                if (!validation(nx, ny, maps)) {
+                if (nx == end[0] && ny == end[1]) {
+                    return dist + 1;
+                }
+                
+                // 통로가 아니라면 continue;
+                if (nx < 0 || ny < 0 || nx > maps.length - 1 || ny > maps[0].length() - 1) {
                     continue;
                 }
-                int temp = cnt + 1;
                 
-                if (visited[nx][ny] > temp) {
-                    visited[nx][ny] = temp;
-                    q.offer(new Point(nx, ny));
+                if (maps[nx].charAt(ny) == 'X') {
+                    continue;
                 }
+                
+                q.offer(new int[]{nx, ny, dist + 1});
             }
         }
-        
-        return visited[d2[0]][d2[1]];
+        return -1;
     }
-    
-    public boolean validation(int x, int y, String[] maps) {
-        if (x < 0 || y < 0 || x >= maps.length || y >= maps[0].length()) {
-            return false;
-        }
-        
-        if (maps[x].charAt(y) == 'X') {
-            return false;
-        }
-        return true;
-    }
-    
-    class Point {
-        int x;
-        int y;
-        
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    // j를 i라 적고, cnt + 1을 cnt++로 하였따..
 }
