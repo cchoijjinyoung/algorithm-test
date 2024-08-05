745bda2e3405900e28a48584665ddf150efd7e5d
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-05 10:20:14 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.23 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,58 +1,73 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
+    
     public int solution(String[] board) {
         int answer = 0;
-        int[] R = new int[2];
-        int[] G = new int[2];
-
+        int[] sp = new int[2];
+        int[] gp = new int[2];
+        
+        // 시작점과 골을 찾는다.
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length(); j++) {
-                if (board[i].charAt(j) == 'R') {
-                    R[0] = i; R[1] = j;
-                } else if (board[i].charAt(j) == 'G') {
-                    G[0] = i; G[1] = j;
+            char[] arr = board[i].toCharArray();
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == 'R') {
+                    sp[0] = i; sp[1] = j;
+                } else if (arr[j] == 'G') {
+                    gp[0] = i; gp[1] = j;
                 }
             }
         }
+        
+        boolean[][] visited = new boolean[board.length][board[0].length()];
+        
+        // 시작점을 큐에 넣는다.
         Queue<int[]> q = new LinkedList<>();
-        q.offer(R);
+        q.offer(new int[]{sp[0], sp[1], 0});
         
-        int[] dx = new int[]{-1, 0, 1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-        int[][] visited = new int[board.length][board[0].length()];
+        // 방문배열에 체크한다.
+        visited[sp[0]][sp[1]] = true;
         
+        // bfs를 돌린다. 멈춘 지점에서 4방향으로 움직인다.
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int dist = visited[cur[0]][cur[1]];
             
-            if (cur[0] == G[0] && cur[1] == G[1]) {
-                return dist;
-            }
+            int nx;
+            int ny;
+            int dist = cur[2];
             
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0];
-                int ny = cur[1];
-                
+                nx = cur[0];
+                ny = cur[1];
+                // 다음 위치가 돌이나 벽일 때까지 움직인다.
                 while (true) {
                     nx += dx[i];
                     ny += dy[i];
+                
+                    if (nx < 0 || ny < 0 || nx > board.length - 1 || ny > board[0].length() - 1) {
+                        break;
+                    }
                     
-                    if (nx < 0 || ny < 0 || 
-                        nx > board.length - 1 || ny > board[0].length() - 1 || 
-                        board[nx].charAt(ny) == 'D') {
-                        break;  
+                    if (board[nx].charAt(ny) == 'D') {
+                        break;
                     }
                 }
-                
+                // 돌이나 벽의 전 위치에 서있음.
                 nx -= dx[i];
                 ny -= dy[i];
                 
-                if (visited[nx][ny] > 0) {
+                if (nx == gp[0] && ny == gp[1]) {
+                    return dist + 1;
+                }
+            
+                if (visited[nx][ny]) {
                     continue;
                 }
-                visited[nx][ny] = dist + 1;
-                q.offer(new int[]{nx, ny});
+                
+                q.offer(new int[]{nx, ny, dist + 1});
+                visited[nx][ny] = true;
             }
         }
         
