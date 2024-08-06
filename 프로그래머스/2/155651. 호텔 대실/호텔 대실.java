71f146a1ba7b1d90741e66e4e570d4382da4652b
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-06 11:03:59 +0900

    [level 2] Title: 호텔 대실, Time: 32.72 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -3,82 +3,41 @@ import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        List<Stack<Reserve>> list = new ArrayList<>();
-        PriorityQueue<Reserve> pq = new PriorityQueue<>(
-            (r1, r2) -> r1.in - r2.in
-        );
-
-        // 북타임을 입실시간 기준 오름차순으로 정렬
+        
+        // 시작 시간으로 정렬
+        Arrays.sort(book_time, (b1, b2) -> b1[0].compareTo(b2[0]));
+        
+        // 방의 끝시간을 담을 리스트
+        List<Integer> list = new ArrayList<>();
+        
         for (int i = 0; i < book_time.length; i++) {
-            String[] time = book_time[i];
-
-            int in = transfer(time[0]);
-            int out = transfer(time[1]);
-
-            pq.add(new Reserve(in, out));
-
-        }
-
-        while (!pq.isEmpty()) {
-            // 북타임 순회
-            Reserve cur = pq.poll();
-
-            int minIdx = -1;
-            int minDiff = 0;        
-            // 리스트에 스택이 존재하면, 리스트를 순회해서,
-            for (int i = 0; i < list.size(); i++) {
-                // 스택의 peek을 확인해서 peek.out이 cur.in과 가장 가까운 스택을 찾아준다.
-                Stack<Reserve> stack = list.get(i);
-                Reserve peek = stack.peek();
-                if (peek.out + 10 > cur.in) {
-                    continue;
-                }
-
-                int diff = peek.out - cur.in;
-                if (diff < minDiff) {
-                    minDiff = diff;
-                    minIdx = i;
+            int start = calc(book_time[i][0]);
+            int end = calc(book_time[i][1]);
+            
+            boolean flag = false;
+            for (int j = 0; j < list.size(); j++) {
+                // 이미 있는 방에 들어가면,
+                if (list.get(j) + 10 <= start) {
+                    list.set(j, end);
+                    flag = true;
+                    break;
                 }
             }
-
-            // 들어갈 스택이 없다면
-            if (minIdx == -1 || list.isEmpty()) {
-                // 새로운 스택을 생성해서 넣어준다.
-                Stack<Reserve> stack = new Stack<>();
-                stack.push(cur);
-                list.add(stack);
-                continue;
+            // 새로운 방을 배정받으면,
+            if (!flag) {
+                list.add(end);
             }
-
-            Stack stack = list.get(minIdx);
-            stack.push(cur);
+            
+            Collections.sort(list, Collections.reverseOrder());
         }
-        answer = list.size();
-        for (int i = 0; i < list.size(); i++) {
-            Stack<Reserve> stack = list.get(i);
-            while (!stack.isEmpty()) {
-                Reserve reserve = stack.pop();
-                System.out.print(reserve.in + "~" + reserve.out + " ");
-            }
-            System.out.println();
-        }
-        return answer;
+        
+        // 하나씩 꺼내면서, 큐에 넣을건데, 입실 가능한 큐이면서, 청소를 가장 마지막에 한 곳
+        
+        return list.size();
     }
-
-    public int transfer(String time) {
-        int result = 0;
-        String[] arr = time.split(":");
-        result = (Integer.parseInt(arr[0]) * 60) + Integer.parseInt(arr[1]);
-        return result;
-    }
-
-    class Reserve {
-        int in;
-        int out;
-
-        public Reserve(int in, int out) {
-            this.in = in;
-            this.out = out;
-        }
+    
+    public int calc(String timeStr) {
+        String[] splits = timeStr.split(":");
+        return Integer.parseInt(splits[0]) * 60 + Integer.parseInt(splits[1]);
     }
 }
