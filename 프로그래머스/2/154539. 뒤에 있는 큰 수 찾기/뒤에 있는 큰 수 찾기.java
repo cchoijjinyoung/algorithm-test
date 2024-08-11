981cc31537e71fb7077b94a02c50a5e575437765
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-11 13:55:57 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 142.17 ms, Memory: 204 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -2,28 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        Stack<Num> stack = new Stack<>();
         int[] answer = new int[numbers.length];
+        // Stack 사용
+        // 스택에 numbers[i]를 넣으면서, 다음 numbers[i]와 stack의 peek()을 비교, 
+        // peek이 더 작으면 answer의 해당 인덱스 위치를 numbers[i]로 바꾼다.
+        // 그러려면 Stack에 인덱스도 같이 push 해줘야한다.
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{numbers[0], 0}); // 숫자와 인덱스
         
-        Arrays.fill(answer, -1);
-        for (int i = 0; i < numbers.length; i++) {
-            while (!stack.isEmpty() && stack.peek().value < numbers[i]) {
-                int idx = stack.pop().idx;
-                answer[idx] = numbers[i];
+        for (int i = 1; i < numbers.length; i++) {
+            while (!stack.isEmpty() && stack.peek()[0] < numbers[i]) {
+                int[] cur = stack.pop();
+                answer[cur[1]] = numbers[i];
             }
-            stack.push(new Num(i, numbers[i]));
+            stack.push(new int[]{numbers[i], i});
         }
         
+        while (!stack.isEmpty()) {
+            int[] cur = stack.pop();
+            answer[cur[1]] = -1;
+        }
         return answer;
-    }
-    
-    class Num {
-        int idx;
-        int value;
-        
-        public Num(int idx, int value) {
-            this.idx = idx;
-            this.value = value;
-        }
     }
 }
