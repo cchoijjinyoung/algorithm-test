3be3a5c2ae8ef1e16bbbf075d59365e1d12c6795
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-14 10:56:02 +0900

    [level 2] Title: 점 찍기, Time: 19.66 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,9 +1,16 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        for (int i = 0; i <= d; i += k) { // x 축
-            double y = Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2)); // 현재 x에 대한 최대 y좌표
-            answer += (long) (y / k) + 1;
+        
+        for (int i = 0; ; i++) {
+            int x = i * k;
+            
+            if (x > d) {
+                break;
+            }
+            
+            double y = Math.sqrt(Math.pow((long)d, 2) - Math.pow((long)x , 2));
+            answer += (long)y / k + 1;
         }
         return answer;
     }
