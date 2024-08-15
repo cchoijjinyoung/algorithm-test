c9744636056cb2f043a7aab37e2e808c67eb2289
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-15 12:16:16 +0900

    [level 2] Title: 택배상자, Time: 229.08 ms, Memory: 181 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,36 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        int idx = 0;
-        for (int i = 1; i <= order.length; i++) {
-            // 못올리면 스택에 넣는다.
-            stack.push(i);
-            
-            // 보조 컨테이너도 확인한다.
-            while (!stack.isEmpty()) {
-                if (stack.peek() == order[idx]) {
-                    stack.pop();
-                    idx++;
+        // order 배열을 변환시켜줌(43125 -> 34215)
+        int[] temp = new int[order.length];
+
+        for (int i = 0; i < order.length; i++) {
+            int idx = order[i] - 1;
+            temp[idx] = i + 1;
+        }
+        // 컨테이너 벨트
+        Queue<Integer> cb = new LinkedList<>();
+        for (int i : temp) {
+            cb.add(i);
+        }
+
+        // 보조 컨테이너 벨트
+        Stack<Integer> sb = new Stack<>();
+
+        // 기대하는 다음 택배상자 번호
+        int expectNumber = 1;
+
+        while (cb.size() > 0 || sb.size() > 0) {
+            if (cb.size() == 0 && sb.peek() != expectNumber) {
+                break;
+            }
+            // 보조 컨벨 확인
+            if (sb.size() > 0 && sb.peek() == expectNumber) {
+                sb.pop();
+                answer++;
+                expectNumber++;
+                continue;
+            }
+
+            // 컨벨 확인
+            if (cb.size() > 0) {
+                if (cb.peek() == expectNumber) {
+                    cb.poll();
                     answer++;
-                    continue;
+                    expectNumber++;
                 } else {
-                    break;
+                    sb.push(cb.poll());
                 }
             }
         }
-        
-        // while (!stack.isEmpty()) {
-        //     if (stack.peek() == order[idx]) {
-        //         stack.pop();
-        //         idx++;
-        //         answer++;
-        //     } else {
-        //         break;
-        //     }
-        // }
+
         return answer;
     }
 }
