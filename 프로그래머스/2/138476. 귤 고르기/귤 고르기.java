b8fefbd23302cfdaaab5305651701cb0f786903a
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-15 11:03:38 +0900

    [level 2] Title: 귤 고르기, Time: 71.30 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,42 +4,25 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
+        // int[] {크기, 갯수}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[1] - x[1]);
         Map<Integer, Integer> hm = new HashMap<>();
         
-        // 크기, 갯수
-        PriorityQueue<Gyool> pq = new PriorityQueue<>(
-        (g1, g2) -> g2.count - g1.count);
-        
         for (int i = 0; i < tangerine.length; i++) {
-            hm.put(tangerine[i], hm.getOrDefault(tangerine[i], 0) + 1);
+            int size = tangerine[i];
+            hm.put(size, hm.getOrDefault(size, 0) + 1);
         }
         
         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            pq.add(new Gyool(entry.getKey(), entry.getValue()));
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
         }
         
-        int count = 0;
-        while (true) {
+        while (k > 0) {
+            int[] cur = pq.poll();
+            k -= cur[1];
             answer++;
-            Gyool cur = pq.poll();
-            count += cur.count;
-            if (count >= k) {
-                break;
-            }
         }
-        // 맵에 크기에 따른 갯수들이 들어가있음.
-        // 갯수가 많은 순서대로 출력
         
         return answer;
-    }
-    
-    class Gyool {
-        int size;
-        int count;
-        
-        public Gyool(int size, int count) {
-            this.size = size;
-            this.count = count;
-        }
     }
 }
