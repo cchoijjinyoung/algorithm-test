3c2c79fc65efe2c995cd879fe99c241b1b7548ef
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-18 23:44:35 +0900

    [level 2] Title: [1차] 캐시, Time: 41.58 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,31 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        // 큐에 도시이름을 순서대로 넣는다.
-        // 넣으면서 큐에 contains인지 확인한다.
-        List<String> list = new ArrayList<>();
+        
+        // LRU : 가장 오랫동안 참조되지 않은 것을 교체
+        List<String> cache = new LinkedList<>();
         
         for (int i = 0; i < cities.length; i++) {
-            String city = cities[i].toLowerCase();
+            String city = cities[i].toUpperCase();
             
-            if (list.contains(city)) {
+            // 캐시에 존재하면
+            if (cache.contains(city)) {
                 answer += 1;
-                list.remove(city);
-                list.add(city);
-            } else {
+                cache.remove(city);
+            } else { // 캐시에 존재하지 않으면
                 answer += 5;
-                if (list.size() < cacheSize) {
-                    list.add(city);
-                } else {
-                    if (!list.isEmpty()) {
-                        list.remove(0);
-                        list.add(city);    
-                    }
+                if (cache.size() >= cacheSize && cacheSize != 0) {
+                    cache.remove(0);
                 }
             }
+            if (cacheSize > 0 && cache.size() < cacheSize) {
+                cache.add(city);    
+            }
         }
-        
-        
         return answer;
     }
 }
