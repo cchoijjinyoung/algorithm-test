f812f47df6d8c52c0fa8949c2110d55dda3b20c4
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-18 16:06:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.26 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,30 @@
 import java.util.*;
 
 class Solution {
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
     public int solution(int[][] maps) {
         int answer = 0;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        
-        int m = maps.length;
-        int n = maps[0].length;
-        
-        maps[m - 1][n - 1] = Integer.MAX_VALUE;
-        
+        int min = Integer.MAX_VALUE;
         boolean[][] visited = new boolean[maps.length][maps[0].length];
-        Queue<int[]> q = new LinkedList<>();
         
-        q.offer(new int[]{0, 0});
+        // 1,1 -> n, m으로 가는 최소 움직임
+        // 0은 벽이다.
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0, 1}); //(0, 0)에 움직임 0으로 시작
+        visited[0][0] = true;
         
         while (!q.isEmpty()) {
-            if (visited[m - 1][n - 1]) {
-                break;
-            }
             int[] cur = q.poll();
-            visited[cur[0]][cur[1]] = true;
+            int x = cur[0];
+            int y = cur[1];
+            int move = cur[2];
             
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if (nx < 0 || ny < 0 || nx > m - 1 || ny > n - 1) { 
+                if (nx < 0 || ny < 0 || nx > maps.length - 1 || ny > maps[0].length -1) {
                     continue;
                 }
                 
@@ -39,16 +36,20 @@ class Solution {
                     continue;
                 }
                 
-                maps[nx][ny] = maps[cur[0]][cur[1]] + 1;
+                if (nx == maps.length - 1 && ny == maps[0].length - 1) {
+                    min = move + 1;
+                }
+                
                 visited[nx][ny] = true;
-                q.offer(new int[]{nx, ny});
+                
+                q.offer(new int[]{nx, ny, move + 1});
             }
         }
-        
-        if (maps[m - 1][n - 1] == Integer.MAX_VALUE) {
-            return -1;
+        if (min == Integer.MAX_VALUE) {
+            answer = -1;
         } else {
-            return maps[m - 1][n - 1];
+            answer = min;
         }
+        return answer;
     }
 }
