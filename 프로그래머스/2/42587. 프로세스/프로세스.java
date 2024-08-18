2d04ebeb6c0e02aad0b8aec251691b3d36dc2e33
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-19 00:56:09 +0900

    [level 2] Title: 프로세스, Time: 1.16 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-        (a, b) -> b - a);
-        
         int answer = 1;
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
         for (int i = 0; i < priorities.length; i++) {
-            pq.add(priorities[i]);
+            pq.offer(priorities[i]);
         }
         
         while (!pq.isEmpty()) {
@@ -24,6 +22,7 @@ class Solution {
                 
             }
         }
+        
         return answer;
     }
 }
