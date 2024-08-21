d488a784c2f76402ac6e762febe49ec5eb4e0051
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-21 22:00:49 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 200 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,86 +1,68 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	static class Node implements Comparable<Node> {
-		int x;
-		int y;
-		int cost;
-		
-		public Node(int x, int y, int cost) {
-			this.x = x;
-			this.y = y;
-			this.cost = cost;
-		}
-		
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
-	}
-	
-	private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	private static int dX[] = {-1, 1, 0, 0};
-	private static int dY[] = {0, 0, -1, 1};
-	private static boolean visited[][];
-	private static int map[][];
-	private static int size[][];
-	
-	private static final int INF = Integer.MAX_VALUE;
-	private static int n, nX, nY;
-	
-	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int cnt = 1;
-		while(true) {
-			n = Integer.parseInt(bf.readLine());
-			if(n == 0) break;
-			
-			map = new int[n][n];
-			visited = new boolean[n][n];
-			size = new int[n][n];
-			
-			for(int i = 0 ; i < n ; i++) {
-				st = new StringTokenizer(bf.readLine());
-				for(int j = 0 ; j < n ; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-					size[i][j] = INF;
-				}
-			}
-			
-			BFS(0, 0, map[0][0]);
-			bw.write("Problem " + cnt + ": " + size[n - 1][n - 1] + "\n");
-			cnt++;
-		}
-		
-		bf.close();
-		bw.flush();
-		bw.close();
-	}
-	
-	private static void BFS(int x, int y, int roopy) {
-		PriorityQueue<Node> Q = new PriorityQueue<>();
-		visited[x][y] = true;
-		Q.offer(new Node(x, y, roopy));
-		while(!Q.isEmpty()) {
-			Node node = Q.poll();
-			for(int i = 0 ; i < 4 ; i++) {
-				nX = node.x + dX[i];
-				nY = node.y + dY[i];
-				
-				if(isValid() && !visited[nX][nY] && size[nX][nY] > (map[nX][nY] + node.cost)) {
-					size[nX][nY] = map[nX][nY] + node.cost;
-					visited[nX][nY] = true;
-					Q.offer(new Node(nX, nY, size[nX][nY]));
-				}
-			}
-		}
-	}
-	
-	private static boolean isValid() {
-		return (nX >= 0 && nY >= 0 && nX < n && nY < n);
-	}
+class Main {
+    static int n;
+    static int[][] board;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int idx = 1;
+        while (true) {
+            n = Integer.parseInt(br.readLine());
+            
+            if (n == 0) {
+                break;
+            }
+            
+            board = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            sb.append("Problem " + idx + ": " + playGame() + "\n");
+            idx++;
+        }
+        System.out.println(sb.toString());
+    }
+    
+    public static int playGame() {
+        int[][] move = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(move[i], Integer.MAX_VALUE);
+        }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[2] - y[2]);
+        
+        pq.offer(new int[]{0, 0, board[0][0]});
+        move[0][0] = board[0][0];
+        
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int cost = cur[2];
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                
+                if (nx < 0 || ny < 0 || nx > n - 1 || ny > n - 1) {
+                    continue;
+                }
+                
+                if (board[nx][ny] + cost < move[nx][ny]) {
+                    move[nx][ny] = board[nx][ny] + cost;
+                    pq.offer(new int[]{nx, ny, move[nx][ny]});
+                }
+            }
+        }
+        return move[n - 1][n - 1];
+    }
 }
