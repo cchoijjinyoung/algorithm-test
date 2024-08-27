88919fd1ed035343f2c1b4193475fe4a4d2f2ca5
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-08-28 01:20:30 +0900

    [Silver V] Title: 올림픽, Time: 128 ms, Memory: 14820 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,46 +1,92 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (x, y) -> {
-                if (x[1] == y[1]) {
-                    if (x[2] == y[2]) {
-                        return y[3] - x[3];
-                    } else {
-                        return y[2] - x[2];
-                    }
+public class Main {
+
+    static class Nation implements Comparable<Nation> {
+        private int name;
+        private int gold;
+        private int silver;
+        private int bronze;
+        private int rank;
+
+        public Nation(int name, int gold, int silver, int bronze) {
+            super();
+
+            this.name = name;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+            this.rank = 1;	// 처음 순위는 1부터 시작
+        } // constructor
+
+        // 갯수가 같으면 금, 은, 동 순으로 내려가면서 비교
+        @Override
+        public int compareTo(Nation o) {
+
+            if(this.gold == o.gold) {
+                if(this.silver == o.silver) {
+                    return o.bronze - this.bronze;
                 } else {
-                    return y[1] - x[1];
-                }
-            }
-        );
-        for (int i = 0; i < N; i++) {
+                    return o.silver - this.silver;
+                } // if-else
+            } else {
+                return o.gold - this.gold;
+            } // if-else
+        } // compareTo
+
+    } // end class:Nation
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        List<Nation> medalList = new ArrayList<>();
+
+        for(int i=1; i<=n; i++) {
+
             st = new StringTokenizer(br.readLine());
-            int idx = Integer.parseInt(st.nextToken());
+
+            int name = Integer.parseInt(st.nextToken());
             int gold = Integer.parseInt(st.nextToken());
             int silver = Integer.parseInt(st.nextToken());
             int bronze = Integer.parseInt(st.nextToken());
-            
-            pq.offer(new int[]{idx, gold, silver, bronze});
-        }
-        
-        int count = 0;
-        while (!pq.isEmpty()) {
-            count++;
-            int[] cur = pq.poll();
-            if (cur[0] == K) {
-                break;
-            }
-        }
-        
-        System.out.println(count);
-    }
-}
+
+            Nation nation = new Nation(name, gold, silver, bronze);
+            medalList.add(nation);
+        } // for
+
+        Collections.sort(medalList);
+
+        for(int i=1; i<n; i++) {
+
+            // 국가 두개씩 비교해서 rank를 정함
+            Nation originN = medalList.get(i-1);
+            Nation nextN = medalList.get(i);
+
+            if(originN.gold == nextN.gold &&
+                originN.silver == nextN.silver &&
+                originN.bronze == nextN.bronze) {
+
+                nextN.rank = originN.rank;
+            } else {
+                nextN.rank = i + 1;
+            } // if-else
+
+        } // for
+
+        medalList.stream().
+            filter(t -> t.name == k).
+            map(t -> t.rank).
+            forEach(System.out::println);
+    } // main
+} // end class
