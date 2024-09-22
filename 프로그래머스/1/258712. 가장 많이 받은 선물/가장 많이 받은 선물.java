bec482075b8ae3dc77af42e2780f5ae7236a8564
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-09-22 21:56:14 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 14.52 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,21 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
+        int len = friends.length;
+        
         // 각각의 친구들에 대해 id를 부여
         Map<String, Integer> idMap = new HashMap<>();
         
-        for (int i = 0; i < friends.length; i++) {
+        for (int i = 0; i < len; i++) {
             idMap.put(friends[i], i);
         }
         
         // 각자 각 친구마다 몇 개의 선물을 줬는지 이차원 배열로 표현
-        int[][] map = new int[friends.length][friends.length];
+        int[][] map = new int[len][len];
         
         // 각자 총 몇 개의 선물을 줬는지 표현
-        int[] give = new int[friends.length];
+        int[] give = new int[len];
         
         // 각자 총 몇 개의 선물을 받았는지 표현
-        int[] take = new int[friends.length];
+        int[] take = new int[len];
         
         for (int i = 0; i < gifts.length; i++) {
             String[] splited = gifts[i].split(" ");
@@ -31,24 +33,24 @@ class Solution {
             take[receiverId]++;
         }
         
-        int[] expect = new int[friends.length];
+        int[] expect = new int[len];
         
-        for (int i = 0; i < friends.length; i++) {
-            for (int j = i; j < friends.length; j++) {
+        for (int i = 0; i < len; i++) { // i 친구가
+            for (int j = i; j < len; j++) { // j 친구에게
                 int f1 = map[i][j];
                 int f2 = map[j][i];
                 
-                if (f1 > f2) {
+                if (f1 > f2) { // 선물을 더 많이 줬으면
                     expect[i]++;
-                } else if (f2 > f1) {
+                } else if (f2 > f1) { // 선물을 덜 줬으면
                     expect[j]++;
                 } else { // 같은 갯수의 선물을 주고 받았다면,
                     int v1 = give[i] - take[i];
                     int v2 = give[j] - take[j];
                     
-                    if (v1 > v2) {
+                    if (v1 > v2) { // i가 선물 지수가 높으면
                         expect[i]++;
-                    } else if (v2 > v1) {
+                    } else if (v2 > v1) { // j가 선물 지수가 높으면
                         expect[j]++;
                     }
                 }
