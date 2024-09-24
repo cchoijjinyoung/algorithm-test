4d777af899ac43c5e314ff500ad6e5308e2b29b3
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-09-25 01:18:35 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 7.23 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -4,38 +4,39 @@ class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
         Arrays.sort(timetable);
         
-        int bus = 540; // 첫차 09:00
-        int max = bus + t * (n - 1); // 막차
-        int cnt = 0; // 지금 버스에 몇명탔는지
-        int before = 0; // 마지막에 탄 녀석의 time
-        int idx = 0;
+        int curBus = 540; // 현재 버스 시간(첫 차 09:00)
+        int endBus = curBus + t * (n - 1); // 마지막 차
+        int rideCnt = 0; // 지금 버스에 몇명탔는지
+        int lastCrew = 0; // 마지막에 탄 크루의 time
+        int idx = 0; // 다음에 탈 크루의 포인터
         
         while (n-- > 0) { // 버스 한대씩 보냄
-            cnt = 0;
-            while (idx < timetable.length) {
-                String nextCrew = timetable[idx];
-                if (convert(nextCrew) <= bus) {
+            rideCnt = 0;
+            while (idx < timetable.length) { // 모든 크루 순회
+                int nextCrew = convert(timetable[idx]);
+                if (nextCrew <= curBus) { // 다음 순번의 크루가 현재 버스에 탈 수 있으면,
                     idx++;
-                    cnt++;
-                    before = convert(nextCrew);
-                } else {
+                    rideCnt++;
+                    lastCrew = nextCrew;
+                } else { // 탈 수 없으면,
                     break;
                 }
                 
-                if (cnt == m) {
+                if (rideCnt == m) { // 현재 버스가 꽉 찼으면,
                     break;
                 }
             }
-            bus += t;
+            curBus += t; // 다음 버스 시간으로 업데이트
         }
-        return cnt < m ? revert(max) : revert(before - 1);
+        // 마지막 차에 자리가 남으면, 마지막 차 시간에 오기. 꽉찼으면 마지막 탑승자보다 1분 일찍오기
+        return rideCnt < m ? revert(endBus) : revert(lastCrew - 1);
     }
     
     public int convert(String time) {
         String[] splitedTime = time.split(":");
-        String h = splitedTime[0];
-        String m = splitedTime[1];
-        return Integer.parseInt(h) * 60 + Integer.parseInt(m);
+        int h = Integer.parseInt(splitedTime[0]);
+        int m = Integer.parseInt(splitedTime[1]);
+        return h * 60 + m;
     }
     
     public String revert(int time) {
