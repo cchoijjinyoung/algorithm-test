670547e0def1bc6ac5d58ade27929c2d86318ed8
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-09-25 00:11:37 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 6.74 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
@@ -1,6 +1,6 @@
 # [level 3] [1차] 셔틀버스 - 17678 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 00:11:16
+2024년 09월 25일 00:11:35
 
 ### 문제 설명
 
--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,60 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
         String answer = "";
-        
-        PriorityQueue<String> pq = new PriorityQueue<>();
-        
-        for (int i = 0; i < timetable.length; i++) {
-            pq.offer(timetable[i]);
-        }
-        
-        int bus = 540;
-        int max = bus + t * (n - 1);
-        int cnt = 0;
-        
-        int before = 0;
-        
-        while (n-- > 0) {
-            cnt = 0;
-            while (!pq.isEmpty()) {
-                String nextCrew = pq.peek();
-                if (convert(nextCrew) <= bus) {
-                    pq.poll();
-                    cnt++;
-                    before = convert(nextCrew);
-                } else {
-                    break;
-                }
-                
-                if (cnt == m) {
-                    break;
-                }
-            }
-            bus += t;
-        }
-        
-        if (cnt < m) {
-            answer = revert(max);
-        } else {
-            answer = revert(before - 1);
-        }
-        
         return answer;
-    }
-    
-    public int convert(String time) {
-        String[] splitedTime = time.split(":");
-        String h = splitedTime[0];
-        String m = splitedTime[1];
-        
-        return Integer.parseInt(h) * 60 + Integer.parseInt(m);
-    }
-    
-    public String revert(int time) {
-        int h = time / 60;
-        int m = time % 60;
-        return String.format("%02d:%02d", h, m);
     }
 }
