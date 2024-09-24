354ec4faccd87ebfa09842e8a4b881d256bfa37c
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-09-24 16:10:43 +0900

    [Gold V] Title: 입국심사, Time: 420 ms, Memory: 29876 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -15,24 +15,21 @@ class Main {
         }
         
         Arrays.sort(times);
-        
         long max = (long)M * times[0];
-        
         long left = 0;
         long right = max;
         while (left <= right) {
-            long mid = right - (right - left) / 2;
-            
+            long mid = (right + left) / 2;
             long cnt = 0;
-            boolean bigger = false;
+            boolean isBig = false;
             for (int i = 0; i < times.length; i++) {
                 cnt += mid / times[i];
                 if (cnt >= M) {
-                    bigger = true;
+                    isBig = true;
                     break;
                 }
             }
-            if (bigger) {
+            if (isBig) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
