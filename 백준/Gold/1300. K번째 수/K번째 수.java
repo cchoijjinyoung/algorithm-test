c190d094b3aa80ae94fbafd5eb1361c4d4eb76c6
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-10-11 15:42:53 +0900

    [Gold I] Title: K번째 수, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+  static int N;
+  static int K;
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    K = Integer.parseInt(br.readLine());
+  }
+
+  static boolean check(long mid) {
+    // mid 보다 작은 숫자가 몇개 있냐?
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+      cnt += (int) Math.min(mid / i, N);
+    }
+    return cnt < K;
+  }
+
+  public static void main(String[] args) throws Exception {
+    input();
+    long L = 0; long R = K;
+
+    while (L <= R) {
+      long mid = (L + R) / 2; // i * j
+      if (check(mid)) {
+        L = mid + 1;
+      } else {
+        R = mid - 1;
+      }
+    }
+    System.out.println(L);
+  }
+}
