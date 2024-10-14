e56375a5b491c3c6971959a410c876b6d428637e
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-10-14 16:23:00 +0900

    [Gold IV] Title: 고냥이, Time: 140 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,46 +1,73 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
-    static int N;
-    static String A;
-    static int[] cnt;
-    static int kind;
-    public static void main(String[] args) {
-        int answer = 0;
-        Scanner sc = new Scanner(System.in);
-        N = Integer.parseInt(sc.nextLine());
-        A = sc.nextLine();
-        cnt = new int[26];
-        kind = 0;
-        
-        // 문자열을 순회하면서, 두포인터 알고리즘을 사용할 것
-        // L ~ R : 검사하고 싶은 구간
-        for (int L = 0, R = 0; R < A.length(); R++) {
-            // R번째 문자를 오른쪽에 추가
-            add(A.charAt(R));
-            
-            // 불가능하면, 가능할 때까지 L을 이동
-            while (kind > N) {
-                erase(A.charAt(L++));
-            }
-            // answer 갱신
-            answer = Math.max(answer, R - L + 1);
-        }
-        System.out.println(answer);
+  static int N;
+  static char[] A; // 문자
+  static int[] visited;
+  static int count = 0;
+
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    visited = new int[26]; // 0이 a
+
+    // abbcaccba
+    String s = br.readLine();
+    A = new char[s.length() + 1];
+    for (int i = 1; i <= s.length(); i++) {
+      A[i] = s.charAt(i - 1); // A[1];
     }
-    
-    static void add(char x) {
-        cnt[x - 'a']++;
-        if (cnt[x - 'a'] == 1) {
-            kind++;
-        }
+  }
+
+  static boolean visited(int idx) {
+    return visited[A[idx] - 'a'] > 0;
+  }
+
+  static void visit(int idx) {
+    visited[A[idx] - 'a']++;
+    if (visited[A[idx] - 'a'] == 1) {
+      count++;
     }
-    
-    static void erase(char x) {
-        cnt[x - 'a']--;
-        if (cnt[x - 'a'] == 0) {
-            kind--;
-        }
+  }
+
+  static void leave(int idx) {
+    visited[A[idx] - 'a']--;
+    if (visited[A[idx] - 'a'] == 0) {
+      count--;
     }
+  }
+
+  static void pro() {
+    int answer = 0;
+    int R = 0;
+    for (int L = 1; L < A.length; L++) {
+      while (R + 1 < A.length) {
+        // 다음 알파벳이 처음보는 문자면,
+        if (!visited(R + 1)) {
+          if (count < N) { // 현재 인식 갯수가 N보다 작으면 인식해도됨
+            R++;
+            visit(R);
+          } else {
+            break;
+          }
+        } else { // 다음 알파벳을 인식했었으면,
+          R++;
+          visit(R);
+        }
+      }
+      answer = Math.max(answer, R - L + 1);
+      leave(L);
+    }
+    System.out.println(answer);
+  }
+
+  public static void main(String[] args) throws Exception {
+    input();
+    pro();
+  }
 }
