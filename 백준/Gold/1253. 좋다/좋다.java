58501abdc604c3c29c7d418429c4b87a030134c2
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-10-14 12:02:18 +0900

    [Gold IV] Title: 좋다, Time: 144 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,41 +1,49 @@
 import java.util.*;
 import java.io.*;
-
 class Main {
-    static int N;
-    static int[] A;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+                
+        int N = Integer.parseInt(br.readLine());
+                
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+                
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+                
+        Arrays.sort(arr);         
         int answer = 0;
-        
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = sc.nextInt();
+        for(int i=0; i<N; i++) {
+            int target = arr[i];
+                        
+            int p1 = 0;
+            int p2 = arr.length-1;
+                        
+            while(p1 < p2) {
+                if(p1 == i) {
+                    p1++;
+                    continue;
+                }
+                                
+                if(p2 == i) {
+                    p2--;
+                    continue;
+                }
+                int sum = arr[p1] + arr[p2];
+                                
+                if(sum == target) {
+                    answer++;
+                    break;
+                }else if(sum > target) {
+                    p2--;
+                }else {
+                    p1++;
+                }
+            }          
         }
-        
-        Arrays.sort(A, 1, N + 1);
-        
-        for (int i = 1; i <= N; i++) {
-            if (check(i)) {
-                answer++;
-            }
-        }
-        System.out.println(answer);
-    }
-    
-    static boolean check(int targetIdx) {
-        int L = 1; int R = N;
-        int target = A[targetIdx];
-        while (L < R) {
-            if (L == targetIdx) L++;
-            else if (R == targetIdx) R--;
-            else {
-                if (A[L] + A[R] == target) return true;
-                if (A[L] + A[R] > target) R--;
-                else L++;
-            }
-        }
-        return false;
+        System.out.print(answer);
     }
 }
