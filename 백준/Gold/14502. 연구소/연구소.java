7712bcf9daf6b3498efd12108515fa6dac8641ec
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-10-15 22:15:39 +0900

    [Gold IV] Title: 연구소, Time: 488 ms, Memory: 127368 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,102 +1,144 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-  static final int[] dx = {1, 0 ,-1, 0};
-  static final int[] dy = {0, 1, 0, -1};
-  static int[][] map;
-  static int n, m;
-  static int max = Integer.MIN_VALUE;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    map = new int[n][m];
+    static int N, M, B, ans;
+    static int[][] A, blank;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-    for (int i = 0; i < n; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < m; j++) {
-        map[i][j] = Integer.parseInt(st.nextToken());
-      }
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new int[N + 1][M + 1];
+        blank = new int[N * M + 1][2];
+        visit = new boolean[N + 1][M + 1];
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <= M; j++)
+                A[i][j] = scan.nextInt();
     }
 
-    dfs(0);
+    // 바이러스 퍼뜨리기!!
+    static void bfs() {
+        Queue<Integer> Q = new LinkedList<>();
 
-    System.out.println(max);
-  }
-
-  static void dfs(int wallCount) {
-    if (wallCount == 3) {
-      bfs();
-      return;
-    }
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (map[i][j] == 0) {
-          map[i][j] = 1;
-          dfs(wallCount + 1);
-          map[i][j] = 0;
-        }
-      }
-    }
-  }
-
-  static void bfs() {
-    Queue<int[]> q = new LinkedList<>();
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (map[i][j] == 2) {
-          q.offer(new int[]{i, j});
-        }
-      }
-    }
-
-    int[][] copyMap = new int[n][m];
-
-    for (int i = 0; i < n; i++) {
-      copyMap[i] = map[i].clone();
-    }
-
-    while (!q.isEmpty()) {
-      int[] cur = q.poll();
-      int cx = cur[0];
-      int cy = cur[1];
-      
-      for (int i = 0; i < 4; i++) {
-        int nx = cx + dx[i];
-        int ny = cy + dy[i];
-        
-        if (nx < 0 || ny < 0 || nx > n - 1 || ny > m - 1) {
-          continue;
+        // 모든 바이러스가 시작점으로 가능하니까, 전부 큐에 넣어준다.
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                visit[i][j] = false;
+                if (A[i][j] == 2) {
+                    Q.add(i);
+                    Q.add(j);
+                    visit[i][j] = true;
+                }
+            }
         }
 
-        if (copyMap[nx][ny] == 0) {
-          q.offer(new int[]{nx, ny});
-          copyMap[nx][ny] = 2;
+        // BFS 과정
+        while (!Q.isEmpty()) {
+            int x = Q.poll(), y = Q.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dir[k][0], ny = y + dir[k][1];
+                if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
+                if (A[nx][ny] != 0) continue;
+                if (visit[nx][ny]) continue;
+                visit[nx][ny] = true;
+                Q.add(nx);
+                Q.add(ny);
+            }
         }
-      }
-    }
-    calc(copyMap);
-  }
-  
-  static void calc(int[][] copyMap) {
-    int safeZone = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (copyMap[i][j] == 0) {
-          safeZone++;
-        }
-      }
+
+        // 탐색이 종료된 시점이니, 안전 영역의 넓이를 계산하고, 정답을 갱신한다.
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) for (int j = 1; j <= M; j++) if (A[i][j] == 0 && !visit[i][j]) cnt++;
+        ans = Math.max(ans, cnt);
     }
 
-    max = Math.max(max, safeZone);
-  }
+    // idx 번째 빈 칸에 벽을 세울 지 말 지 결정해야 하고, 이 전까지 selected_cnt 개의 벽을 세웠다.
+    static void dfs(int idx, int selected_cnt) {
+        if (selected_cnt == 3) {  // 3 개의 벽을 모두 세운 상태
+            bfs();
+            return;
+        }
+        if (idx > B) return;  // 더 이상 세울 수 있는 벽이 없는 상태
+
+        A[blank[idx][0]][blank[idx][1]] = 1;
+        dfs(idx + 1, selected_cnt + 1);
+
+        A[blank[idx][0]][blank[idx][1]] = 0;
+        dfs(idx + 1, selected_cnt);
+    }
+
+    static void pro() {
+        // 모든 벽의 위치를 먼저 모아놓자.
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (A[i][j] == 0) {
+                    B++;
+                    blank[B][0] = i;
+                    blank[B][1] = j;
+                }
+            }
+        }
+
+        // 벽을 3개 세우는 모든 방법을 확인해보자!
+        dfs(1, 0);
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
