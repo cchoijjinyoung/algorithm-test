cbdba4f38e02d81f7b47b4a79c809abaca84a9ba
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-11-07 11:18:01 +0900

    [Silver I] Title: 기타리스트, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,57 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-/**
- * 기타리스트, 실버1
- * 알고리즘 : dp
- */
 public class Main {
   static int N, S, M;
   static int[] V;
   static boolean[][] dp;
-
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    N = Integer.parseInt(st.nextToken()); // 곡의 갯수
-    S = Integer.parseInt(st.nextToken()); // Start 볼륨
-    M = Integer.parseInt(st.nextToken()); // Max 볼륨
-
+    N = Integer.parseInt(st.nextToken());
+    S = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
     V = new int[N + 1];
+    dp = new boolean[N + 1][M + 1];
+
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i <= N; i++) {
       V[i] = Integer.parseInt(st.nextToken());
     }
-
-    dp = new boolean[N + 1][M + 1];
   }
 
   static void pro() {
     dp[0][S] = true;
 
-    int answer = 0;
     for (int i = 1; i <= N; i++) {
-      boolean flag = false;
-      answer = 0;
-      for (int prev = 0; prev <= M; prev++) {
-        if (!dp[i - 1][prev]) continue;
+      for (int j = 0; j <= M; j++) {
+        if (dp[i - 1][j]) {
+          int minus = j - V[i];
+          int plus = j + V[i];
 
-        for (int cur : new int[]{prev - V[i], prev + V[i]}) {
-          if (cur < 0 || cur > M) continue;
-          answer = Math.max(answer, cur);
-          dp[i][cur] = true;
-          flag = true;
+          if (minus >= 0) dp[i][minus] = true;
+          if (plus <= M) dp[i][plus] = true;
         }
       }
-      if (!flag) {
-        System.out.println(-1);
+    }
+
+    for (int i = M; i >= 0; i--) {
+      if (dp[N][i]) {
+        System.out.println(i);
         return;
       }
     }
-    System.out.println(answer);
+    System.out.println(-1);
   }
 
   public static void main(String[] args) throws Exception {
