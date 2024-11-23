d1bbcd012e0fc739950838745f3a79a7ea3271d5
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-11-23 09:12:07 +0900

    [Gold I] Title: 계단 수, Time: 124 ms, Memory: 16168 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,58 +1,64 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 계단 수 : 골드1
- */
 public class Main {
   static int N;
-  static int[][][][] Dy;
-  static final int MOD = 1000000000;
+  static int MOD = 1_000_000_000;
+  static int[][][][] dp; // [len][last][min][max]
 
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
-    Dy = new int[N + 1][10][10][10];
+    dp = new int[N + 1][10][10][10];
   }
 
   static void pro() {
-    // 초기값 구하기
-    for (int num = 1; num <= 9; num++) {
-      Dy[1][num][num][num] = 1;
+    int answer = 0;
+
+    for (int i = 1; i <= 9; i++) {
+      dp[1][i][i][i] = 1;
     }
 
-    // 점화식을 토대로 Dy 배열 채우기
     for (int len = 2; len <= N; len++) {
-      for (int prev = 0; prev <= 9; prev++) {
-        for (int low = 0; low <= 9; low++) {
-          for (int high = 0; high <= 9; high++) {
-            for (int cur: new int[]{prev - 1, prev + 1}) {
-              if (cur < 0 || cur > 9) continue;
-              Dy[len][cur][Math.min(low, cur)][Math.max(high, cur)] += Dy[len - 1][prev][low][high];
-              Dy[len][cur][Math.min(low, cur)][Math.max(high, cur)] %= MOD;
+      for (int last = 0; last <= 9; last++) {
+        for (int min = 0; min <= 9; min++) {
+          for (int max = 0; max <= 9; max++) {
+            int up = last + 1;
+            int down = last - 1;
+
+            if (down >= 0) {
+              int update_min = Math.min(min, down);
+              int update_max = Math.max(max, down);
+              dp[len][down][update_min][update_max] += dp[len - 1][last][min][max];
+              dp[len][down][update_min][update_max] %= MOD;
+            }
+
+            if (up <= 9) {
+              int update_min = Math.min(min, up);
+              int update_max = Math.max(max, up);
+              dp[len][up][update_min][update_max] += dp[len - 1][last][min][max];
+              dp[len][up][update_min][update_max] %= MOD;
             }
           }
         }
       }
     }
 
-    // Dy배열로 정답 계산하기
-    int ans = 0;
-    for (int num = 0; num <= 9; num++) {
-      ans += Dy[N][num][0][9];
-      ans %= MOD;
+    for (int last = 0; last <= 9; last++) {
+      answer += dp[N][last][0][9];
+      answer %= MOD;
     }
 
-    System.out.println(ans);
+    System.out.println(answer);
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     input();
     pro();
+
   }
 }
