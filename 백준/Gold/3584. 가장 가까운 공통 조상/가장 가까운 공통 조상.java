7ec419c45c547de9e5a318d38592c53cb08ce83d
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-11-28 10:11:01 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 244 ms, Memory: 26244 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -10,11 +10,17 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+/**
+ * ! 아이디어
+ * 간선정보(부모 -> 자식)를 보고 각 노드별 부모 노드 저장(parents[N + 1])
+ * Queue 에 공통 부모를 찾을 두 노드를 넣고, 시작
+ * 위로 올라가면서 각 부모를 set 에 담음
+ * set 에 이미 존재하는 부모일 시, 최소 공통 부모이다.
+ */
 public class Main {
   static int T, N;
   static int node1, node2;
   static int[] parents;
-  static List<List<Integer>> tree;
 
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,27 +31,18 @@ public class Main {
     for (int i = 0; i < T; i++) {
       N = Integer.parseInt(br.readLine());
       parents = new int[N + 1];
-      initializeTree();
 
       for (int j = 0; j < N - 1; j++) {
         st = new StringTokenizer(br.readLine());
         int parent = Integer.parseInt(st.nextToken());
         int child = Integer.parseInt(st.nextToken());
         parents[child] = parent;
-        // tree.get(child).add(parent);
       }
 
       st = new StringTokenizer(br.readLine());
       node1 = Integer.parseInt(st.nextToken());
       node2 = Integer.parseInt(st.nextToken());
       pro();
-    }
-  }
-
-  static void initializeTree() {
-    tree = new ArrayList<>();
-    for (int node = 0; node <= N; node++) {
-      tree.add(new ArrayList<>());
     }
   }
 
