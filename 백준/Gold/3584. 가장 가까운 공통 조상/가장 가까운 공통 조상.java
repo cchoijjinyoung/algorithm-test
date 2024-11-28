4f941d47636cf6064e53e67c59d8928d6d780f44
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-11-28 09:58:26 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 276 ms, Memory: 29708 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -0,0 +1,74 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int T, N;
+  static int node1, node2;
+  static List<List<Integer>> tree;
+
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      N = Integer.parseInt(br.readLine());
+      initializeTree();
+
+      for (int j = 0; j < N - 1; j++) {
+        st = new StringTokenizer(br.readLine());
+        int parent = Integer.parseInt(st.nextToken());
+        int child = Integer.parseInt(st.nextToken());
+        tree.get(child).add(parent);
+      }
+
+      st = new StringTokenizer(br.readLine());
+      node1 = Integer.parseInt(st.nextToken());
+      node2 = Integer.parseInt(st.nextToken());
+      pro();
+    }
+  }
+
+  static void initializeTree() {
+    tree = new ArrayList<>();
+    for (int node = 0; node <= N; node++) {
+      tree.add(new ArrayList<>());
+    }
+  }
+
+  static void pro() {
+    Set<Integer> set = new HashSet<>();
+    Queue<Integer> q = new LinkedList<>();
+    q.add(node1);
+    q.add(node2);
+    set.add(node1);
+    set.add(node2);
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+
+      if (tree.get(cur).isEmpty()) continue;
+      int parent = tree.get(cur).get(0);
+      if (set.contains(parent)) {
+        System.out.println(parent);
+        return;
+      }
+      set.add(parent);
+      q.add(parent);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    input();
+  }
+}
