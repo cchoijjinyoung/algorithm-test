1208db4223450c26cf031f94e4269ecfffc4506a
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-12-05 11:28:29 +0900

    [Gold IV] Title: 테트로미노, Time: 500 ms, Memory: 30848 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,8 +15,8 @@ import java.util.StringTokenizer;
 public class Main {
   static int N, M, answer;
   static int[][] board;
-  static List<int[][]> initPolys;
-  static Set<int[][]> polys;
+  static List<Poly> initPolys;
+  static Set<Poly> polys;
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
@@ -36,31 +37,31 @@ public class Main {
   }
 
   static void initializePoly() {
-    initPolys.add(new int[][]{
+    initPolys.add(new Poly(new int[][]{
         {1, 1, 1, 1}
-    });
+    }));
 
-    initPolys.add(new int[][]{
+    initPolys.add(new Poly(new int[][]{
         {1, 1},
         {1, 1}
-    });
+    }));
 
-    initPolys.add(new int[][]{
+    initPolys.add(new Poly(new int[][]{
         {1, 0},
         {1, 0},
         {1, 1}
-    });
+    }));
 
-    initPolys.add(new int[][]{
+    initPolys.add(new Poly(new int[][]{
         {1, 0},
         {1, 1},
         {0, 1}
-    });
+    }));
 
-    initPolys.add(new int[][]{
+    initPolys.add(new Poly(new int[][]{
         {1, 1, 1},
         {0, 1, 0}
-    });
+    }));
   }
 
   // 도형 뒤집기(위 - 아래)
@@ -92,23 +93,25 @@ public class Main {
   }
 
   static void pro() {
-    // 도형 초기화
+    // 초기 5개의 도형 초기화
     initializePoly();
 
-    // 모든 도형(대칭 + 회전)에 대해 순회
-    for (int[][] poly : initPolys) {
+    // 초기 도형 5개로 만들 수 있는 모든 도형(대칭 + 회전) 만들기
+    for (Poly p : initPolys) {
+      int[][] poly = p.poly;
       for (int i = 0; i < 2; i++) {
         poly = reverse(poly);
-        polys.add(poly);
+        polys.add(new Poly(poly));
         for (int j = 0; j < 4; j++) {
           poly = turn(poly);
-          polys.add(poly);
+          polys.add(new Poly(poly));
         }
       }
     }
 
-    for (int[][] poly : polys) {
-      // 현재 도형을 모든 격자에 대입
+    // 모든 도형을 보드에 매칭시켜서 최대값을 찾는다.
+    for (Poly p : polys) {
+      int[][] poly = p.poly;
       int poly_x = poly.length;
       int poly_y = poly[0].length;
 
@@ -116,15 +119,16 @@ public class Main {
         for (int y = 0; y < M; y++) {
           int sum = 0;
 
-          // 현재 도형이 오른쪽 혹은 아래 밖으로 나가는 경우
+          // 현재 도형이 아래 밖으로 나가는 경우
           if (x + poly_x > N) {
             continue;
           }
-
+          // 오른쪽 밖으로 나가는 경우
           if (y + poly_y > M) {
             continue;
           }
 
+          // 도형이 격자와 닿는 부분 sum++
           for (int xx = 0; xx < poly_x; xx++) {
             for (int yy = 0; yy < poly_y; yy++) {
               if (poly[xx][yy] == 1) {
@@ -142,5 +146,30 @@ public class Main {
   public static void main(String[] args) throws Exception {
     input();
     pro();
+  }
+}
+
+class Poly {
+  int[][] poly;
+
+  Poly(int[][] poly) {
+    this.poly = poly;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Poly poly1 = (Poly) o;
+    return Arrays.deepEquals(poly, poly1.poly);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.deepHashCode(poly);
   }
 }
