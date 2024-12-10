253eb8255cffff2ed6e439151de18e84ecb46823
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-12-10 10:42:10 +0900

    [Gold V] Title: 동전 2, Time: 120 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,17 +23,14 @@ public class Main {
   }
 
   static void pro() {
-    Arrays.sort(coins);
     Arrays.fill(dp, 10001);
-    for (int coin_idx = coins.length - 1; coin_idx >= 0; coin_idx--) {
-      int coin = coins[coin_idx];
+    for (int coin : coins) {
       if (coin > K) continue;
       dp[coin] = 1;
       for (int target = coin + 1; target <= K; target++) {
           if (dp[target - coin] == 0) {
             continue;
           }
-          
           dp[target] = Math.min(dp[target], dp[target - coin] + 1);
       }
     }
