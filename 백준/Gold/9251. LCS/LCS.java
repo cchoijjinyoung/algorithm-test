0736d88c5295acfbcefffe784d03d02d5826ac4b
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-12-30 10:31:30 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18208 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static char[] str1, str2;
+  static int[][] dp;
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    str1 = br.readLine().toCharArray();
+    str2 = br.readLine().toCharArray();
+    dp = new int[str1.length + 1][str2.length + 1];
+  }
+
+  static void pro() {
+    for (int i = 1; i <= str1.length; i++) {
+      for (int j = 1; j <= str2.length; j++) {
+        if (str1[i - 1] == str2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+    System.out.println(dp[str1.length][str2.length]);
+  }
+
+  public static void main(String[] args) throws Exception {
+    input();
+    pro();
+  }
+}
