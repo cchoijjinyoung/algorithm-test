51ab0d3ca60a96a710cb64ade9678915ccf39369
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-01-27 13:40:53 +0900

    [Gold IV] Title: 타일 채우기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static int N;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    input(br);
+    pro();
+  }
+
+  static void input(BufferedReader br) throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+  }
+
+  static void pro() {
+    int[] DP = new int[N + 1];
+
+
+    if (N % 2 != 0) {
+      System.out.println(0);
+      return;
+    }
+
+    DP[0] = 1;
+    DP[1] = 0;
+    DP[2] = 3;
+
+    for (int i = 4; i <= N; i += 2) {
+      DP[i] = DP[i - 2] * 3;
+      // 4, 6, 8, ...
+      for (int j = i - 4; j >= 0; j -= 2) {
+        DP[i] += DP[j] * 2;
+      }
+    }
+
+    // 결과값 출력
+    System.out.println(DP[N]);
+  }
+}
