1afda18ea677c8f6699ff71284fa00f294edabb6
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-02-02 10:30:36 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 232 ms, Memory: 76848 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+  static String A, B;
+  static int[][] dp;
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    A = br.readLine();
+    B = br.readLine();
+  }
+
+  static void pro() {
+    int answer = 0;
+    dp = new int[A.length() + 1][B.length() + 1];
+    
+    for (int i = 1; i <= A.length(); i++) {
+      for (int j = 1; j <= B.length(); j++) {
+        if (A.charAt(i - 1) == B.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+          answer = Math.max(answer, dp[i][j]);
+        }
+      }
+    }
+    System.out.println(answer);
+  }
+
+  public static void main(String[] args) throws Exception {
+    input();
+    pro();
+  }
+}
