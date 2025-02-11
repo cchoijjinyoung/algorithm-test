a5ee086364755ce353b31bfef3384888087b04f5
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-02-11 10:12:33 +0900

    [level 1] Title: 평균 구하기, Time: 1.13 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,12 +1,10 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        double sum = 0;
-        for (int i : arr) {
-            sum += i;
-        }
         
-        answer = sum / arr.length;
-        return answer;
+        int sum = Arrays.stream(arr).sum();
+        
+        return (double) sum / arr.length;
     }
 }
