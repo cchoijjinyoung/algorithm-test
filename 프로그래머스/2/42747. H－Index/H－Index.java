f49f4e7d6767cc01a2bbebc3d0bbe18a41197a99
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-05-13 11:48:34 +0900

    [level 2] Title: H-Index, Time: 0.89 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,16 +4,17 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         
+        // 정렬해서, index + 1이 현재 value와 같거나 작으면 break 후 return
         Arrays.sort(citations);
         
-        int len = citations.length;
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= len) {
-                answer = len;
-                break;
+        int count = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            int value = citations[i];
+            count++;
+            if (count > value) {
+                return count - 1;
             }
-            len--;
         }
-        return answer;
+        return citations.length;
     }
 }
