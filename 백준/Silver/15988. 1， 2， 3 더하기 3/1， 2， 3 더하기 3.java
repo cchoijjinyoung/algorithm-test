c7774849a01f76fba95c84c500de37e5b6eb4733
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-05-17 10:31:21 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 136 ms, Memory: 22176 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,44 +1,31 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int T, MOD;
-  static int[] N;
   static long[] dp;
-
-  static void input() throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    T = Integer.parseInt(br.readLine());
-    N = new int[T];
-    for (int i = 0; i < T; i++) {
-      N[i] = Integer.parseInt(br.readLine());
-    }
-
+  static int T;
+  static int MOD = 1_000_000_009;
+  public static void main(String[] args) throws IOException {
     dp = new long[1000001];
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
-  }
 
-  static void pro() {
-    MOD = 1_000_000_009;
-
-    for (int i = 4; i <= 1000000; i++) {
-      long sum = dp[i - 3] + dp[i - 2] + dp[i - 1];
-      dp[i] = sum % MOD;
+    for (int i = 4; i < 1000001; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD;
     }
 
-    for (int i = 0; i < N.length; i++) {
-      System.out.println(dp[N[i]]);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    T = Integer.parseInt(br.readLine());
+    for (int i = 0; i < T; i++) {
+      int n = Integer.parseInt(br.readLine());
+      System.out.println(dp[n]);
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-    input();
-    pro();
   }
 }
