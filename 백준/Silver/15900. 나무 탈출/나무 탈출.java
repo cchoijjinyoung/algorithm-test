6bb3fcaf4e6287e9b8f7ead31037093778e76aa5
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-05-24 22:53:53 +0900

    [Silver I] Title: 나무 탈출, Time: 880 ms, Memory: 225320 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -1,80 +1,66 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-/**
- * 나무 탈출 : 실버1
- */
 public class Main {
   static int N;
-  static List<List<Integer>> tree;
-
-  static void input() throws IOException {
+  static List<List<Integer>> graph;
+  static boolean[] visited;
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
 
-    tree = new ArrayList<>();
+    graph = new ArrayList<>();
     for (int i = 0; i <= N; i++) {
-      tree.add(new ArrayList<>());
+      graph.add(new ArrayList<>());
     }
+    visited = new boolean[N + 1];
 
     for (int i = 0; i < N - 1; i++) {
       st = new StringTokenizer(br.readLine());
       int from = Integer.parseInt(st.nextToken());
       int to = Integer.parseInt(st.nextToken());
 
-      tree.get(from).add(to);
-      tree.get(to).add(from);
+      graph.get(from).add(to);
+      graph.get(to).add(from);
     }
+
+    solve();
   }
 
-  static void pro() {
-    int root = 1;
-    int sum = 0;
-    Set<Integer> visited = new HashSet<>();
+  public static void solve() {
+    int result = 0;
     Queue<int[]> q = new LinkedList<>();
-    q.add(new int[]{root, 0});
-    visited.add(root);
+    q.offer(new int[]{1, 0});
+    visited[1] = true;
 
     while (!q.isEmpty()) {
-      int[] cur = q.poll();
-      int now = cur[0];
-      int level = cur[1];
+      int[] info = q.poll();
+      int cur = info[0];
+      int depth = info[1];
 
-      if (now != root && tree.get(now).size() == 1) {
-        sum += level;
-        continue;
+      int count = 0;
+      for (int next : graph.get(cur)) {
+        if (visited[next]) continue;
+        q.offer(new int[]{next, depth + 1});
+        visited[next] = true;
+        count++;
       }
-
-      for (int next : tree.get(now)) {
-        if (visited.contains(next)) {
-          continue;
-        }
-        q.add(new int[]{next, level + 1});
-        visited.add(next);
+      if (count == 0) {
+        result += depth;
       }
     }
-
-    if (sum % 2 == 0) {
+    if (result % 2 == 0) {
       System.out.println("No");
     } else {
       System.out.println("Yes");
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-    input();
-    pro();
-
   }
 }
