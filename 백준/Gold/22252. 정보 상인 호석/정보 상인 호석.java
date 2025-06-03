8520deb9526269ea970b9c1cc022960b585bedd0
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-06-03 10:31:43 +0900

    [Gold V] Title: 정보 상인 호석, Time: 1076 ms, Memory: 146156 KB -BaekjoonHub

--- 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.java
+++ 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,55 +7,37 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int Q;
   static long answer;
+  static int Q;
   static Map<String, PriorityQueue<Integer>> map;
-
-  static void input() throws IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    Q = Integer.parseInt(br.readLine());
+    Q = Integer.parseInt(st.nextToken());
     map = new HashMap<>();
-
     for (int i = 0; i < Q; i++) {
       st = new StringTokenizer(br.readLine());
       int type = Integer.parseInt(st.nextToken());
       String name = st.nextToken();
+      if (!map.containsKey(name)) {
+        map.put(name, new PriorityQueue<>((i1, i2) -> i2 - i1));
+      }
 
+      PriorityQueue<Integer> pq = map.get(name);
       if (type == 1) {
-        if (!map.containsKey(name)) {
-          map.put(name, new PriorityQueue<>((o1, o2) -> o2 - o1));
-        }
-
         int k = Integer.parseInt(st.nextToken());
         for (int j = 0; j < k; j++) {
-          int number = Integer.parseInt(st.nextToken());
-          map.get(name).add(number);
+          pq.offer(Integer.parseInt(st.nextToken()));
+        }
+      } else if (type == 2) {
+        int b = Integer.parseInt(st.nextToken());
+        while (b-- > 0 && !pq.isEmpty()) {
+          answer += pq.poll();
         }
       }
-      else {
-        int count = Integer.parseInt(st.nextToken());
-        if (map.containsKey(name)) {
-          PriorityQueue<Integer> pq = map.get(name);
-          int size = pq.size();
-          count = Math.min(count, size);
-
-          while (count-- > 0 && !pq.isEmpty()) {
-            answer += pq.poll();
-          }
-        }
-      }
+      map.put(name, pq);
     }
-  }
-
-  static void pro() {
     System.out.println(answer);
-  }
-
-  public static void main(String[] args) throws Exception {
-    input();
-    pro();
-
   }
 }
