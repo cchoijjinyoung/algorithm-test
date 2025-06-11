de21adb91992b0bab54fcc524fdad427111e744f
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-06-11 10:11:32 +0900

    [Silver IV] Title: 수 찾기, Time: 1060 ms, Memory: 60144 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,62 +2,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
   static int N, M;
-  static int[] A, B;
-
+  static Set<Integer> set = new HashSet<>();
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
 
     N = Integer.parseInt(br.readLine());
     st = new StringTokenizer(br.readLine());
-    A = new int[N];
     for (int i = 0; i < N; i++) {
-      A[i] = Integer.parseInt(st.nextToken());
+      set.add(Integer.parseInt(st.nextToken()));
     }
 
     M = Integer.parseInt(br.readLine());
     st = new StringTokenizer(br.readLine());
-    B = new int[M];
     for (int i = 0; i < M; i++) {
-      B[i] = Integer.parseInt(st.nextToken());
-    }
-  }
-
-  static void binarySearch(int number) {
-    // A 배열에서 이분 탐색
-    int L = 0; int R = A.length - 1;
-
-    while (L <= R) {
-      int mid = (L + R) / 2;
-
-      if (A[mid] == number) {
+      if (set.contains(Integer.parseInt(st.nextToken()))) {
         System.out.println(1);
-        return;
-      } else if (A[mid] > number) {
-        R = mid - 1;
       } else {
-        L = mid + 1;
+        System.out.println(0);
       }
     }
-    System.out.println(0);
   }
 
   static void pro() {
-    // 이분 탐색
-    Arrays.sort(A);
-    for (int j : B) {
-      binarySearch(j);
-    }
+
   }
 
   public static void main(String[] args) throws Exception {
     input();
     pro();
-
   }
 }
