0672225079643928054fc00c63de04eb04fccdb8
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2025-07-14 11:35:50 +0900

    [Gold IV] Title: 부분합, Time: 264 ms, Memory: 24052 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,48 +4,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 부분합 골드4
- * 투 포인터
- */
-class Main {
-  static int N;
-  static int S;
-  static int[] A;
-
+public class Main {
+  static int N, S;
+  static int[] arr;
   static void input() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
     S = Integer.parseInt(st.nextToken());
-
-    A = new int[N + 1];
+    arr = new int[N];
     st = new StringTokenizer(br.readLine());
-    for (int i = 1; i <= N; i++) {
-      A[i] = Integer.parseInt(st.nextToken());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
   }
 
   static void pro() {
-    int R = 0; int sum = 0; int answer = N + 1;
-
-    for (int L = 1; L <= N; L++) {
-      sum -= A[L - 1];
-
-      while (R + 1 <= N && sum < S) {
-        R++;
-        sum += A[R];
-      }
-
+    int result = Integer.MAX_VALUE;
+    int sum = 0;
+    int L = 0; int R = 0;
+    while (true) {
       if (sum >= S) {
-        answer = Math.min(answer, R - L + 1);
+        result = Math.min(result, R - L);
+        sum -= arr[L++];
+      } else if (R == N) {
+        break;
+      } else {
+        sum += arr[R++];
       }
     }
-    if (answer == N + 1) {
-      answer = 0;
+
+    if (result == Integer.MAX_VALUE) {
+      System.out.println(0);
+    } else {
+      System.out.println(result);
     }
-    System.out.println(answer);
   }
 
   public static void main(String[] args) throws Exception {
